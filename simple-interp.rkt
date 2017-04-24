94495f97bd03bcdbcd3a89f8981406c07e3795a0
Michael Ballantyne <michael.ballantyne@gmail.com>
2017-04-24 16:12:07 -0600

    add some primitives to simple interp

--- simple-interp.rkt
+++ simple-interp.rkt
@@ -6,39 +6,54 @@
 
 (define evalo
   (lambda (expr val)
-    (eval-expro expr '() val)))
+    (fresh ()
+      (absento 'closure expr)
+      (eval-expro expr '() val))))
 
 (define eval-expro
   (lambda (expr env val)
     (conde
-      ((fresh (rator rand x body env^ a)
+      [(numbero expr) (== expr val)]
+      [(fresh (rator rand x body env^ a)
          (== `(,rator ,rand) expr)
          (eval-expro rator env `(closure ,x ,body ,env^))
          (eval-expro rand env a)
-         (eval-expro body `((,x . ,a) . ,env^) val)))
-      ((fresh (x body)
+         (eval-expro body `((,x . ,a) . ,env^) val))]
+      [(fresh (x body)
          (== `(lambda (,x) ,body) expr)
          (symbolo x)
          (== `(closure ,x ,body ,env) val)
-         (not-in-envo 'lambda env)))
-      ((symbolo expr) (lookupo expr env val)))))
+         (not-in-envo 'lambda env))]
+      [(symbolo expr) (lookupo expr env val)]
+      [(fresh (e res d)
+         (== `(car ,e) expr)
+         (== `(,val . ,d) res)
+         (not-in-envo 'car env)
+         (eval-expro e env res))]
+      [(fresh (e res a)
+         (not-in-envo 'car env)
+         (== `(cdr ,e) expr)
+         (== `(,a . ,val) res)
+         (eval-expro e env res))])))
 
 (define not-in-envo
   (lambda (x env)
     (conde
-      ((== '() env))
-      ((fresh (y v rest)
+      [(== '() env)]
+      [(fresh (y v rest)
          (== `((,y . ,v) . ,rest) env)
          (=/= y x)
-         (not-in-envo x rest))))))
+         (not-in-envo x rest))])))
 
 (define lookupo
   (lambda (x env t)
     (conde
-      ((fresh (y v rest)
+      [(fresh (y v rest)
          (== `((,y . ,v) . ,rest) env) (== y x)
-         (== v t)))
-      ((fresh (y v rest)
+         (== v t))]
+      [(fresh (y v rest)
          (== `((,y . ,v) . ,rest) env) (=/= y x)
-         (lookupo x rest t))))))
+         (lookupo x rest t))])))
 
+(run 1 (q)
+  (evalo '(((lambda (x) (lambda (y) x)) 5) 6) q))
